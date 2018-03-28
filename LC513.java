a61fdfcefb175163cf4d8d6223a8b2aa5005097f
dylu0410 <dingyinglu0410@gmail.com>
2018-03-28 16:31:03 -0400

    LC513

---
+++ LC513.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null)
+            return -1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int res = -1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (i == 0)
+                    res = cur.val;
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+        }
+        return res;
+    }
+}
