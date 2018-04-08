ba68c2721206559b141f87a44b52e81ef5e5d1da
dylu0410 <dingyinglu0410@gmail.com>
2018-04-08 11:56:55 -0400

    Remove K digits, commit second version with less time

--- LC402.java
+++ LC402.java
@@ -20,4 +20,25 @@ class Solution {
         res = res.substring(i, i + len > res.length()? res.length() : i + len);
         return res.length() == 0? "0" : res;
     }
+
+    //Version 2: use array as the stack, time complexity improved because using the stack might be time consuming
+    //Conrner case: "10", k = 2, "9", k = 1
+    //Time: O(n), Space: O(n)
+    public String removeKdigits2(String num, int k) {
+        char[] stk = new char[num.length()];
+        int top = 0;
+        int count = num.length() - k;
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);
+            while (top > 0 && stk[top - 1] > c && k > 0) {
+                top--;
+                k--;
+            }
+            stk[top++] = c;
+        }
+        int idx = 0;
+        while (idx < count && stk[idx] == '0')
+            idx++;
+        return idx == count? "0" : new String(stk, idx, count - idx);
+    }
 }
